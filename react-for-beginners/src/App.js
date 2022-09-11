3f32f5d9a34f120efaa4e0df25da774e513d3a04
yoon ji hoon <koom@utplus.co.kr>
2022-09-12 01:11:39 +0900

    7.3 end

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,49 +1,45 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(() => {console.log('first app lenderd')}, [])
 
-  const [toDo, setToDo] = useState('');
-  const [toDoList, setToDoList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  function onChange(e){
-    setToDo(e.target.value);
+  const getMovies = async () => {
+    const response = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)).json()
+    setMovies(response.data.movies);
+    setLoading(false); 
   }
 
-  function onSubmit(e){
-    console.log('onSubmit in');
-    e.preventDefault();
-    if(toDo === '') return;
-    if(toDoList.findIndex((toDoRecorded) => toDoRecorded === toDo) !== -1) {setToDo(''); return};
-    setToDoList((cur) => [...cur, toDo]);
-    setToDo('');
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const Movies = () => {
+    console.log('movies: ', movies);
+    return (
+      <div>
+        {
+          movies.map((movie) => 
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <img src={movie.medium_cover_image} />
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(g => <li key={g}>{g}</li>)}
+              </ul>
+            </div>
+          )
+        }
+      </div>
+    )
   }
 
-  function finishJob(idx){
-    console.log('finishJob in');
-    setToDoList((cur) => {cur.splice(idx, 1); return cur})
-  }
-
+  console.log('movies: ', movies);
 
   return (
     <div>
-      <h1>My Todo List !! total: {toDoList.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="dododo" />
-        <button>submit</button>
-      </form>
-      <hr />
-      <div>
-        {
-          toDoList.map((todo, idx) => {
-            return <div key={`todo-wrapper-${idx}`}>
-              <span key={`todo-${idx}`}>{todo}</span>
-              {/* <button key={`finish-btn-${idx}`} onClick={finishJob(idx)}>done!</button> */}
-            </div>
-
-          })
-        }
-      </div>
+      {loading ? <h1>Loading... </h1> : <Movies />}
     </div>
   );
 }
