a63471a8ff0e4de79734521d78d89d86bfaec96c
yoon ji hoon <koom@utplus.co.kr>
2022-09-12 00:50:27 +0900

    7.1 end

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,21 +1,49 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const changeShowState = () => setShowing((cur) => !cur)
-  const Hello = () => {
-    useEffect(() => {
-      console.log('im created');
-      return () => console.log('im destroyed');
-    }, []);
-    return (
-      <div><h1>Hello world!</h1></div>
-    )
+  useEffect(() => {console.log('first app lenderd')}, [])
+
+  const [toDo, setToDo] = useState('');
+  const [toDoList, setToDoList] = useState([]);
+
+  function onChange(e){
+    setToDo(e.target.value);
   }
+
+  function onSubmit(e){
+    console.log('onSubmit in');
+    e.preventDefault();
+    if(toDo === '') return;
+    if(toDoList.findIndex((toDoRecorded) => toDoRecorded === toDo) !== -1) {setToDo(''); return};
+    setToDoList((cur) => [...cur, toDo]);
+    setToDo('');
+  }
+
+  function finishJob(idx){
+    console.log('finishJob in');
+    setToDoList((cur) => {cur.splice(idx, 1); return cur})
+  }
+
+
   return (
     <div>
-      <button onClick={changeShowState}> {showing ? 'hide' : 'show'}</button>
-      {showing ? <Hello /> : null}
+      <h1>My Todo List !! total: {toDoList.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="dododo" />
+        <button>submit</button>
+      </form>
+      <hr />
+      <div>
+        {
+          toDoList.map((todo, idx) => {
+            return <div key={`todo-wrapper-${idx}`}>
+              <span key={`todo-${idx}`}>{todo}</span>
+              {/* <button key={`finish-btn-${idx}`} onClick={finishJob(idx)}>done!</button> */}
+            </div>
+
+          })
+        }
+      </div>
     </div>
   );
 }
